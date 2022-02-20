c3b12ada9137c712914b5ea57478064e747f846f
NPitroff <nicolai.pitroff@gmail.com>
2022-02-20 13:25:01 -0800

    Setup the server to run and print the homepage

--- app.js
+++ app.js
@@ -7,4 +7,16 @@ const bodyParser = require("body-parser");
 // translate to app
 const app = express();
 // use bodyParser
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Test webpage to make sure it is linked
+app.get("/", function(req, res){
+  res.send("<h1>Hello World!</h1>");
+})
+
+
+
+// turn on server
+app.listen(3000, function(){
+  console.log("Running on port 3000")
+})
