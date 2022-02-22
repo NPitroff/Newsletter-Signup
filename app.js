4acb755020947a4169251e5f470cf83d5c50930e
NPitroff <nicolai.pitroff@gmail.com>
2022-02-22 07:01:37 -0800

    Installed axios

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 //require https for requests
 const https = require("https");
+// require axios
+const axios = require('axios').default;
 // require body-parser to sort through the data
 const bodyParser = require("body-parser");
 // translate to app
@@ -9,9 +11,9 @@ const app = express();
 // use bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 // use jsdom to mock a window for jquery
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
+// const { JSDOM } = require( "jsdom" );
+// const { window } = new JSDOM( "" );
+// const $ = require( "jquery" )( window );
 
 // script to use the css and images on the website
 app.use(express.static("public"));
