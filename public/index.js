86ac17042a140baef9a4b5dc92fe94b0e90fd8b4
NPitroff <nicolai.pitroff@gmail.com>
2022-02-23 07:32:54 -0800

    Added mailchimp url and api key

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
 // require axios
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
 //define the params for the form
 const firstName = document.getElementById("firstname");
 const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
+//api key for mailchimp
+apiKey = "104c8bf74096f338abec0ce79043d3fa-us14";
+//mailchimp url
+url = "https://us14.api.mailchimp.com/3.0/ping"
 
 
 
