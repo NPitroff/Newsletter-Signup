faa26febae5051b4ba3a7f30fbb2cb643edc701d
NPitroff <nicolai.pitroff@gmail.com>
2022-02-23 16:06:35 -0800

    reset the project code to get rid of bloatware

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 // npm packages
 const express = require("express");
-// require express-formidable
-const formidable = require('express-formidable');
 //require https for requests
 const https = require("https");
 // require body-parser to sort through the data
 const bodyParser = require("body-parser");
+// require request
+const request = require("request");
 // translate to app
 const app = express();
 
 
 // use bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
-// use jsdom to mock a window for jquery
-// const { JSDOM } = require( "jsdom" );
-// const { window } = new JSDOM( "" );
-// const $ = require( "jquery" )( window );
+
 
 // script to use the css and images on the website
 app.use(express.static("public"));
@@ -23,6 +20,12 @@ app.use(express.static("public"));
 // Test webpage to make sure it is linked
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/",function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
 })
 
 
--- public/index.js
+++ public/index.js
@@ -1,39 +1,14 @@
-// require axios
-// const axios = require('axios').default;
-
-//define the params for the form
-const firstName = document.getElementById("firstname");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
 //api key for mailchimp
-apiKey = "104c8bf74096f338abec0ce79043d3fa-us14";
-//mailchimp url
-url = "https://us14.api.mailchimp.com/3.0/ping"
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-
-
-// axios get request
-axios.get('https://regres.in/api/users')
-  .then(function(response) {
-    // handle for success
-    console.log(response);
-  })
-  .catch(function(error){
-    // handle error
-    console.log(error)
-  });
-
-// axios post route
-axios.post("https://regres.in/api/users", {
-  firstName:'Bob',
-  lastName:'Hope',
-  email:'bob.hope@aol.com'
-}
-  )
-.then(function(response){
-
-  console.log("POST response "+response);
-})
-.catch(function(error){
-  console.log("POST error "+error);
+mailchimp.setConfig({
+  apiKey: "95d75a985e150ba3d0938dd3010cadfe-us14",
+  server: "us14",
 });
+
+async function run() {
+  const response = await mailchimp.ping.get();
+  console.log(response);
+}
+
+run();
